fa4c12016a9a2ff903ab5ce3f22fafadfc0a43e4
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-10-26 11:01:56 -0500

    updated algorithm

--- arraysandstrings/checkPermutations.js
+++ arraysandstrings/checkPermutations.js
@@ -46,34 +46,55 @@ if window's length is equal to s1.length. If so, we return true. Else, we advanc
 3. As we reduce size of window, our window may no longer contain all characters of s1.
 Then, we go back to step(1) and repeat the process. We do this until the end of array.
 */
-var checkInclusion = function(s1, s2) {
-    if (s1 === "" || s2 === "") {
-        return false;
-    }
-    let m = new Map();
-    // Record every character of s1 to Hash table with entry being
-    // (character, number of occurrences)
-    for (let i = 0; i < s1.length; i++) {
-        m.set(s1[i], m.get(s1[i]) + 1 || 1);
-    }
-    let start = 0, windowSize = s1.length;
-    // number of unique characters to collect
-    let counter = m.size;
-    for (let end = 0; end < s2.length; end++) {
-        let char = s2[end];
-        if (m.has(char)) m.set(char, m.get(char) - 1);
-        if (m.get(char) === 0) counter--; // we collected all occurrences of this char
-        // we collected all occurrences of every character in s1
-        while (counter === 0) {
-            if (end-start+1 === windowSize) return true;
-            if (m.has(s2[start])) m.set(s2[start], m.get(s2[start]) + 1);
-            if (m.get(s2[start]) === 1) counter++; // we should collect one more unique char
-            start++;
-        }
-    }
-    return false;
-    // T.C: O(M+N), M = length of s1, N = length of s2
-    // S.C: O(M)
+var checkInclusion = function (s1, s2) {
+	// If s1 is larger than s2 then match is not possible
+	// i.e (s1 cannot be substring of s2)
+	if (s1.length > s2.length) return false;
+	let neededChar = {}; //To Store the frequency/count of required string s1
+	for (let i = 0; i < s1.length; i++) {
+		// Initially neededChar[s1[i]] will be undefined and
+		// undefined || 0 will return 0. So we increment it by 1
+		neededChar[s1[i]] = (neededChar[s1[i]] || 0) + 1;
+	}
+	/* 
+	Now we have neededChar
+	i.e neededChar={
+		a:1,
+		b:1
+	}
+	*/
+	let left = 0, //left pointer/index of the sliding window
+		right = 0, //right pointer/index of the sliding window
+		requiredLength = s1.length; //length of the substring required in s2
+
+	// Now iterate until the right index of window is lesser than length of s2
+	while (right < s2.length) {
+		// If we found s2 character in s1 i.e in neededChar then we decrease requiredLength
+		if (neededChar[s2[right]] > 0) requiredLength--;
+		// Since we have encountered new char i.e s2[right] we decrease it's 
+		// count in neededChar even if it is not present in neededChar because we only care about neededChars
+		neededChar[s2[right]]--;
+		right++ //window is incremented by 1 step
+
+		// Now if our requiredLength becomes 0 it means we have found a match of the s2 substring
+		// So we return true
+		if (requiredLength === 0) return true;
+
+		// If our window length is equal to s1 length (length of string to search in s2)
+		// then we have to remove left element of window i.e left++ and add new element from right 
+		// will be added in next iteration
+		if (right - left === s1.length) {
+			// if the left element we are removing was a required character then we increase requiredLength
+			// because that element will no longer be the part of sliding window
+			if (neededChar[s2[left]] >= 0) requiredLength++;
+			// We will also increase the count of left element removed from window
+			neededChar[s2[left]]++;
+			// And finally we will decrease the window size by 1 from left i.e left++
+			left++
+		}
+	}
+	// If match was not found we return false
+	return false;
 };
 
 console.log(checkInclusion("yahi", "hiya")); //true
