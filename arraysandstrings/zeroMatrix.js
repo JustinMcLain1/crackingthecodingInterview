f4f8abb8353e57a0b3baa1c72e8e585286972677
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-11-05 14:12:03 -0500

    got working algo

--- arraysandstrings/zeroMatrix.js
+++ arraysandstrings/zeroMatrix.js
@@ -10,42 +10,77 @@ E: 0 2
 
 */
 
+const _matrix = [[4,1,3],[2,-4,0],[5,9,2]];
 
-let zeroMatrix = (mat) => {
+
+const zeroingMat = (matrix, n)=> {
+    //O(n^2)
+    for(let r=0; r < n; r++){
+        for(let c=0; c < n; c++){
+            if(matrix[r][c]===0) {
+                matrix[r][c] = true;
+            }
+        }
+    }
+    //find locations we made true and zero them
+    //O(n^3)
+    for(let r=0; r < n; r++){
+        for(let c=0; c < n; c++){
+            if(matrix[r][c]===true) {
+                //zero the rows
+                for(let i=0; i < n; i++){
+                    //change the number of the row and setting the column to zero
+                    matrix[r][i] = 0
+                }
+                //zero the column
+                for(let i=0; i < n; i++){
+                    //change the number of the column and setting the row to zero
+                    matrix[i][c] = 0
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+console.log(zeroingMat(_matrix,3));
+
+// const mat = [[2,4,3],[2,-4,0],[1,3,2]];
+
+// //O(n^2)
+// const zeroMatrix = (mat,n) => {
 
     
-    let firstZeroRow;
-    let firstZeroColumn;
+//     let firstZeroRow;
+//     let firstZeroColumn;
 
-    for(let i = 0; i < mat.length; i++) {
-        for(let j = 0; j < mat[0].length; j++) {
-            if(mat[i][j] === 0) {
-                if(i===0) firstZeroRow = true;
-                if(j===0) firstZeroColumn = true;
-                mat[i][0] = 0;
-                mat[0][j] = 0;
-            }
+//     for(let i = 0; i < n.length; i++) {
+//         for(let j = 0; j < n[0].length; j++) {
+//             if(mat[i][j] === 0) {
+//                 if(i===0) firstZeroRow = true;
+//                 if(j===0) firstZeroColumn = true;
+//                 mat[i][0] = 0;
+//                 mat[0][j] = 0;
+//             }
 
-        }
+//         }
 
-    }
-    for(let i = 1; i < mat.length; i++){
-        for(let j=1; j < mat.length; j++) {
-            if(mat[i][0] === 0 || mat[0][j] === 0) {
-                mat[i][j] = 0;
-            }
-        }
-    }
-    if(firstZeroRow) {
-        mat[0].fill(0);
-    }
-    if(firstZeroColumn) {
-        for(let i = 0; i < mat.length; i++) {
-            mat[i][0]=0;
-        }
-    }
+//     }
+//     for(let i = 1; i < n.length; i++){
+//         for(let j=1; j < n.length; j++) {
+//             if(mat[i][0] === 0 || mat[0][j] === 0) {
+//                 mat[i][j] = 0;
+//             }
+//         }
+//     }
+//     if(firstZeroRow) {
+//         mat[0].fill(0);
+//     }
+//     if(firstZeroColumn) {
+//         for(let i = 0; i < n.length; i++) {
+//             mat[i][0]=0;
+//         }
+//     }
 
-    return console.log(zeroMatrix(mat));
-};
-
-console.log(zeroMatrix([1,2] , [3,4]));
+// };
+// return console.log(zeroMatrix(mat,3));
